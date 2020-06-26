e3164a004c47ef0d69f835b79505af5ab7c55241
dragomagic21 <67450714+dragomagic21@users.noreply.github.com>
2020-06-26 16:57:18 +0300

    Add files via upload
    Add Linear Regression, Linear and Quad Discriminant

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt  # Отрисовка графиков
 import numpy as np  # Numpy
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-#from sklearn.preprocessing import StandardScaler, MinMaxScaler #Нормировщики
+from sklearn.preprocessing import StandardScaler, MinMaxScaler #Нормировщики
 
 # Get number in lottery
-data = pd.read_csv('/home/vadim/python_loto/loto_stat.csv', encoding='cp1251', header=0, index_col=0)
+data = pd.read_csv('D:\\temp\\loto_stat.csv', encoding='cp1251', header=0, index_col=0)
 print(range(len(data)))
 print('############')
 
 data['SUM'] = data.sum(axis=1)
-print('DATA WITH SUM')
-print(data)
-print('############ /n')
+#print('DATA WITH SUM')
+#print(data)
+#print('############ /n')
 data['lottery_num'] = data.index
 
 # Append SUM of numbers in each lottery
@@ -23,20 +23,14 @@ for i in range(1, len(data)):
         res.loc[data['lottery_num'].iloc[i]] = 1
     else:
         res.loc[data['lottery_num'].iloc[i]] = -1
-#res = []
-#for i in range(1, len(data)):
-#    if (data['SUM'].iloc[i] > data['SUM'].iloc[i - 1]):
-#        res.append(1)
-#    else:
-#        res.append(-1)
+res = res[1:]
 
+#print('RESULT COLUM')
+#print(len(res))
+#print(res)
+#print('#################')
 
-print('RESULT COLUM')
-print(len(res))
-print(res)
-print('#################')
-
-plt.plot(res)
+plt.plot(data['SUM'])
 plt.savefig('demo.png', bbox_inches='tight')
 
 # Make new Data_Model - get indexing from previous
@@ -48,37 +42,60 @@ data_for_model['SUM_prc'] = data['SUM'].pct_change()*100.0
 for i,x in enumerate(data_for_model['SUM_prc']):
     if (abs(x) < 0.0001):
         data_for_model['SUM_prc'][i] = 0.0001
-#res.reset_index(inplace=True)
 
 x = data_for_model
-y = res
+y = res.values.ravel()
+y = y.astype('int')
 
 x_train = x[1:98]
-print(x_train)
 x_test = x[99:]
-#print(x_test)
 
 y_train = y[1:98]
-#print(y_train)
-#print('##### y_TRAIN #######')
-#print(y_train)
-y_test = y[99:]
-#print(y_test)
+y_test = y[98:]
 
-d = pd.DataFrame(index=data_for_model.index)
-d['FACT'] = res[1:]
-d.drop(d.index[0], inplace=True)
-#print(d.head())
+d = pd.DataFrame(index=x_test.index) # Set for check model
+d['FACT'] = res[1:] # Real values
 
-#print(y_train.values.ravel())
+# Visual
+print('##########')
+print('##########')
 
+
+# Логистическая регрессия
 from sklearn.linear_model import LogisticRegression
 model1 = LogisticRegression()
 model1.fit(x_train, y_train)     # Обучение (подбор параметров модели)
-#d['Predict_LR'] = model1.predict(x_test) # Тест
+
+d['Predict_LR'] = model1.predict(x_test) # Тест
+# Считаем процент правильно предсказанных направлений изменения цены:
+d["Correct_LR"] = (1.0+d['Predict_LR']*d['FACT'])/2.0
+print(d)
+hit_rate1 = np.mean(d["Correct_LR"])
+print("Процент верных предсказаний: %.1f%%" % (hit_rate1*100))
+
+
+
+# Линейный дискриминант
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+model2 = LDA()
+model2.fit(x_train, y_train)     # Обучение (подбор параметров модели)
+d['Predict_LDA'] = model2.predict(x_test) # Тест
 
 # Считаем процент правильно предсказанных направлений изменения цены:
-#d["Correct_LR"] = (1.0+d['Predict_LR']*d['FACT'])/2.0
-#print(d)
-#hit_rate1 = np.mean(d["Correct_LR"])
-#print("Процент верных предсказаний: %.1f%%" % (hit_rate1*100))
+d["Correct_LDA"] = (1.0+d['Predict_LDA']*d["FACT"])/2.0
+print(d)
+hit_rate2 = np.mean(d["Correct_LDA"])
+print("Процент верных предсказаний: %.1f%%" % (hit_rate2*100))
+
+
+# Квадратичный дискриминант
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
+model3 = QDA()
+model3.fit(x_train, y_train)     # Обучение (подбор параметров модели)
+d['Predict_QDA'] = model3.predict(x_test) # Тест
+
+# Считаем процент правильно предсказанных направлений изменения цены:
+d["Correct_QDA"] = (1.0+d['Predict_QDA']*d["FACT"])/2.0
+print(d)
+hit_rate3 = np.mean(d["Correct_QDA"])
+print("Процент верных предсказаний: %.1f%%" % (hit_rate3*100))
